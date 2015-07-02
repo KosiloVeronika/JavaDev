a9b69fe655a6ff3ddba8694f9e8c6caec8aa1d36
Veronika Kosilo <Kosia_nika@mail.ru>
2015-07-03 00:00:16 +0300

    final
    multithreading

--- multithreading/src/com/epam/multithreading/create/Creator.java
+++ multithreading/src/com/epam/multithreading/create/Creator.java
@@ -11,15 +11,15 @@ public class Creator {
 	public static Library CreateLibrary() {
 		HashMap<Integer, Boolean> lib = new HashMap<Integer, Boolean>(9);
 		ArrayList<Book> bookList = new ArrayList<Book>(9);
-		bookList.add(new Book(1,"Лолита","Набоков",true));
-		bookList.add(new Book(2,"Похороните меня за плинтусом","Павел Санаев",true));
-		bookList.add(new Book(3,"Гарри поттер и филосовский камень","Роулинг",true));
-		bookList.add(new Book(4,"Удушье","Чак Паланик",true));
-		bookList.add(new Book(5,"Триумфальная арка","Ремарк",true));
-		bookList.add(new Book(6,"На западном фронте без перемен","Ремарк",false));
-		bookList.add(new Book(7,"Голова профессора Доуэля","Inkognito",true));
-		bookList.add(new Book(8,"Таинственный остров","Жюль Верн",false));
-		bookList.add(new Book(9,"Избранные Романы","Александр Беляев",false));
+		bookList.add(new Book(1, "\"Лолита\"", "Владимир Владимирович Набоков", true, false));
+		bookList.add(new Book(2, "\"Похороните меня за плинтусом\"", "Павел Санаев", true, false));
+		bookList.add(new Book(3, "\"Бойцовский клуб\"", "Чак Паланик", true, false));
+		bookList.add(new Book(4, "\"Удушье\"", "Чак Паланик", true, false));
+		bookList.add(new Book(5, "\"Триумфальная арка", "Э.М.Ремарк", true, false));
+		bookList.add(new Book(6, "\"На западном фронте без перемен\"", "Э.М.Ремарк", false, false));
+		bookList.add(new Book(7, "\"Голова профессора Доуэля\"", "Александр Беляев", true , false));
+		bookList.add(new Book(8, "\"Таинственный остров\"","Жюль Верн", false, false));
+		bookList.add(new Book(9, "\"Избранные Романы\"", "Александр Беляев", false, false));
 		for(int i = 0; i < bookList.size(); i++) {
 			lib.put(bookList.get(i).getID(), true);
 		}
